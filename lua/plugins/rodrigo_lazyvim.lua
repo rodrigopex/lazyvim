df64bab93a0cf6176946a7855d8f148dbf3ad11f
Rodrigo Peixoto <rodrigopex@gmail.com>
2023-08-28 14:19:17 -0300

    Change lazy file
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

--- lua/plugins/rodrigo_lazyvim.lua
+++ lua/plugins/rodrigo_lazyvim.lua
@@ -1,5 +1,8 @@
 return {
     {
+        "kkoomen/vim-doge",
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         keys = {
             { "<leader>fs", "<cmd>write<cr>", desc = "Save file" },
@@ -221,6 +224,60 @@ return {
                 rust = "nvim-test.runners.cargo-test",
                 typescript = "nvim-test.runners.jest",
                 typescriptreact = "nvim-test.runners.jest",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                -- Ensure mason installs the server
+                clangd = {
+                    keys = {
+                        {
+                            "<leader>cR",
+                            "<cmd>ClangdSwitchSourceHeader<cr>",
+                            desc = "Switch Source/Header (C/C++)",
+                        },
+                    },
+                    root_dir = function(fname)
+                        return require("lspconfig.util").root_pattern(
+                            "Makefile",
+                            "configure.ac",
+                            "configure.in",
+                            "config.h.in",
+                            "meson.build",
+                            "meson_options.txt",
+                            "build.ninja"
+                        )(fname) or require("lspconfig.util").root_pattern(
+                            "compile_commands.json",
+                            "compile_flags.txt"
+                        )(fname) or require("lspconfig.util").find_git_ancestor(
+                            fname
+                        )
+                    end,
+                    capabilities = {
+                        offsetEncoding = { "utf-16" },
+                    },
+                    cmd = {
+                        "clangd",
+                        "--header-insertion=never",
+                    },
+                    init_options = {
+                        usePlaceholders = true,
+                        completeUnimported = true,
+                        clangdFileStatus = true,
+                    },
+                },
+            },
+            setup = {
+                clangd = function(_, opts)
+                    local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+                    require("clangd_extensions").setup(
+                        vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts })
+                    )
+                    return false
+                end,
             },
         },
     },

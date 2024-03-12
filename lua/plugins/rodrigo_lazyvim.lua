f2906a3408b4c341af9d34365a9e31e3a472a245
Rodrigo Peixoto <rodrigopex@gmail.com>
2024-03-12 07:57:53 -0300

    Improve support for protobuf files
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

--- lua/plugins/rodrigo_lazyvim.lua
+++ lua/plugins/rodrigo_lazyvim.lua
@@ -232,6 +232,24 @@ return {
         opts = {
             servers = {
                 -- Ensure mason installs the server
+                pyright = {},
+                ruff_lsp = {
+                    keys = {
+                        {
+                            "<leader>co",
+                            function()
+                                vim.lsp.buf.code_action({
+                                    apply = true,
+                                    context = {
+                                        only = { "source.organizeImports" },
+                                        diagnostics = {},
+                                    },
+                                })
+                            end,
+                            desc = "Organize Imports",
+                        },
+                    },
+                },
                 clangd = {
                     keys = {
                         {
@@ -256,6 +274,7 @@ return {
                             fname
                         )
                     end,
+                    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
                     capabilities = {
                         offsetEncoding = { "utf-16" },
                     },
@@ -278,7 +297,64 @@ return {
                     )
                     return false
                 end,
+                ruff_lsp = function()
+                    require("lazyvim.util").lsp.on_attach(function(client, _)
+                        if client.name == "ruff_lsp" then
+                            -- Disable hover in favor of Pyright
+                            client.server_capabilities.hoverProvider = false
+                        end
+                    end)
+                end,
             },
         },
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = function()
+            local plugin = require("lazy.core.config").plugins["conform.nvim"]
+            -- if plugin.config ~= M.setup then
+            --     Util.error({
+            --         "Don't set `plugin.config` for `conform.nvim`.\n",
+            --         "This will break **LazyVim** formatting.\n",
+            --         "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
+            --     }, { title = "LazyVim" })
+            -- end
+            ---@class ConformOpts
+            local opts = {
+                -- LazyVim will use these options when formatting with the conform.nvim formatter
+                format = {
+                    timeout_ms = 3000,
+                    async = false, -- not recommended to change
+                    quiet = false, -- not recommended to change
+                },
+                ---@type table<string, conform.FormatterUnit[]>
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    fish = { "fish_indent" },
+                    sh = { "shfmt" },
+                    proto = { "buf" },
+                    options = { "buf" },
+                    python = { "autopep8" },
+                },
+                -- The options you set here will be merged with the builtin formatters.
+                -- You can also define any custom formatters here.
+                ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+                formatters = {
+                    injected = { options = { ignore_errors = true } },
+                    -- # Example of using dprint only when a dprint.json file is present
+                    -- dprint = {
+                    --   condition = function(ctx)
+                    --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+                    --   end,
+                    -- },
+                    --
+                    -- # Example of using shfmt with extra args
+                    -- shfmt = {
+                    --   prepend_args = { "-i", "2", "-ci" },
+                    -- },
+                },
+            }
+            return opts
+        end,
     },
 }

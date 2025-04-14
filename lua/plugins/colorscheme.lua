90cd59d73999a0e8faa1352b97d85527431fb6c0
Rodrigo Peixoto <rodrigopex@gmail.com>
2025-04-14 16:19:12 -0300

    Add gruvbox colorscheme
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
     { "navarasu/onedark.nvim", opts = { style = "cool" } },
     { "rebelot/kanagawa.nvim", opts = { style = "lotus" } },
+    { "ellisonleao/gruvbox.nvim", opts = { background = "dark" } },
+    { "catppuccin/nvim", name = "catppuccin", opts = { style = "frappe" } },
+
     -- Configure LazyVim to load gruvbox
     {
         "LazyVim/LazyVim",
         opts = {
-            -- colorscheme = "onedark",
-            colorscheme = "kanagawa-wave",
+            colorscheme = "gruvbox",
+            -- colorscheme = "kanagawa-wave",
         },
     },
 }
